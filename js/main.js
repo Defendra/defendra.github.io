3f07c2580c78471a205c65b572d79a9ab7162c44
Defendra <158090878+Defendra@users.noreply.github.com>
2024-09-29 08:01:37 +0100

    adding top banner

--- js/main.js
+++ js/main.js
@@ -129,3 +129,23 @@ toggles.forEach((toggle) => {
     toggle.parentElement.classList.toggle("active");
   });
 });
+
+
+
+
+$( ".menu-off" ).click(function() {
+    $( this ).removeClass( "menu-off" );
+    $( this ).addClass( "menu-on" );
+   $('.floating-button-menu-close').addClass('menu-on');
+  });
+  $('.floating-button-menu-close').click(function(){
+    $( this ).addClass( "menu-off" );
+    $( this ).removeClass( "menu-on" );
+    $('.floating-button-menu').toggleClass('menu-on');
+  });
+
+
+    // JavaScript to close the footer
+    function closeFooter() {
+    document.getElementById('privacy-footer').style.display = 'none';
+}
